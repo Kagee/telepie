9f0351d2d63b54dfaf765e2458586fd6ca89e7bf
Anders Einar Hilden <hildenae@gmail.com>
2013-06-11 00:22:33 +0200

    Convert to Maven project, import commons-io and commons-lang

--- src/no/hild1/bank/utils/CleanSDCTelepay.java
+++ src/no/hild1/bank/utils/CleanSDCTelepay.java
@@ -4,37 +4,31 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringEscapeUtils;
 
 public class CleanSDCTelepay {
 
-	private static String cleanHTML(String path) throws IOException {
-		String source = readFile(path, Charset.forName("ISO 8859-1"));
+	private static boolean cleanHTML(String path) throws IOException {
+		String source = FileUtils.readFileToString(new File(path), "ISO_8859_1");
 		
 		Pattern p = Pattern.compile("(<pre>)(.+?)(</pre>)"); //"^[a-zA-Z]+([0-9]+).*");
 		Matcher m = p.matcher(source);
 
 		if (m.find()) {
 			String tpData = m.group(2);
-		    //
-			tpData=tpData.replace("&Oslash;", "Ø");
-			tpData=tpData.replace("&oslash;", "ø");
-			tpData=tpData.replace("&Aring;", "Å");
-			tpData=tpData.replace("&aring;", "å");
-			tpData=tpData.replace("<br />", "\n");
-		    return tpData;
+			tpData = StringEscapeUtils.unescapeHtml4(tpData).replace("<br />", "\n");
+			FileUtils.writeStringToFile(new File(path+".telepay"), tpData, "ISO_8859_1");
+			return true;
 		}
-		return "No match";
-	}
-
-	private static String readFile(String path, Charset encoding) throws IOException {
-		/* http://stackoverflow.com/a/326440*/
-		byte[] encoded = Files.readAllBytes(Paths.get(path));
-		return encoding.decode(ByteBuffer.wrap(encoded)).toString();
+		return false;
 	}
 
 	/**
@@ -42,12 +36,15 @@ public class CleanSDCTelepay {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		String[] files = {"testfile1.htm" };
+		String[] files = {"testfile1.htm", "testfile2.htm" };
 		String path = "Telepay-filer/SDC/";
-		//for (int i = 0; i <= files.length; i++) {	
-			System.out.println(cleanHTML(path + files[0]));
-			System.out.println(cleanHTML(path + files[1]));
-		//}
+		for (int i = 0; i < files.length; i++) {	
+			if (!cleanHTML(path + files[i])) {
+				System.out.println("Kunne ikke vaske " + files[i]);
+			} else {
+				System.out.println("Vasket " + files[i]);
+			}
+		}
 	}
 
 }
